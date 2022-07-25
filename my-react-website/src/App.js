29b22dc806c8de54ac186e66fbace40d494018fe
rishabhmishra9340 <104819756+rishabhmishra9340@users.noreply.github.com>
2022-07-25 16:13:20 +0530

    first commit

---
+++ my-react-website/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css';
+import Resturant from "./menufiles/Resturant";
+
+
+
+
+
+ function App() {
+  return (
+    <div>
+     <Resturant/>
+    </div>
+    
+    
+  )
+}
+
+
+  
+
+ export default App;
